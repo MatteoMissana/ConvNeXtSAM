e76632c0bc9a9ada136c560d99d3e21afa976525
Francesco Leni <francesco.leni@mail.polimi.it>
2023-11-16 17:49:29 +0100

    script per provare ad importare un modello

---
+++ prova_import.py
@@ -0,0 +1,52 @@
+import torch
+# from utils.general import non_max_suppression
+import cv2 as cv
+from matplotlib import  pyplot as plt
+
+#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------     IMPORTANTE    ----------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+#
+#     i pesi che vuoi usare devono essere nel main branch della tua repo locale
+
+
+
+def draw_bbox(preds, img):
+    for pred in preds:
+        for bbox in pred:
+            obj_class = int(bbox[-1])
+            conf = float(bbox[-2])
+
+            p1 = (int(bbox[0]),int(bbox[1]))
+            p2 = (int(bbox[2]),int(bbox[3]))
+
+            img = cv.rectangle(img, p1, p2, (255, 0, 0), thickness=2)
+
+    return img
+
+
+model = torch.hub.load('MatteoMissana/ConvNeXtSAM', 'custom', 'ConvNext.pt', trust_repo=True)
+
+# print(model)
+# summary(model, (3, 640, 640))
+
+img = plt.imread('dataset/micro/images/test/image_000002_png.rf.a9ecda8477278f9747c4d47ffb8038bb.jpg')
+
+out = model(img)  # out è un oggetto Detections dichiarato in common.py righa 1950
+
+# out.save()
+pred = out.pred
+# pred == list(tensor(x_up_left, y_up_left, width, height, conf, class); len(pred) == num_imgs; tensor.size() == (n_preds,6)
+
+print(pred)
+
+
+# out_img = draw_bbox(pred, img)
+#
+# fig, ax = plt.subplots()
+# ax.imshow(out_img)
+#
+# plt.savefig('runs/detect/exp/img.jpg')
+# plt.close()
+
+
